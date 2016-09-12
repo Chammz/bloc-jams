afe815105a40ee94ea7c3bbedfada14147fce5d3
Chammz <topher.hammond@gmail.com>
2016-09-12 07:37:31 -0700

    Checkpoint complete. Added eventlisteners to songitemrow's for play/pause button

--- scripts/album.js
+++ scripts/album.js
@@ -48,7 +48,7 @@
 var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
-      + '  <td class="song-item-number">' + songNumber + '</td>'
+      + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
       + '  <td class="song-item-title">' + songName + '</td>'
       + '  <td class="song-item-duration">' + songLength + '</td>'
       + '</tr>'
@@ -57,7 +57,7 @@ var createSongRow = function(songNumber, songName, songLength) {
      return template;
  };
 
-     // Select elements that we want to populate with text dumaically
+     // Select elements that we want to populate with text dynamically
      var albumTitle = document.getElementsByClassName('album-view-title')[0];
      var albumArtist = document.getElementsByClassName('album-view-artist')[0];
      var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
@@ -81,6 +81,13 @@ var createSongRow = function(songNumber, songName, songLength) {
      }
  };
  
+//Element's we'll be adding listeners to
+ var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+ var songRows = document.getElementsByClassName('album-view-song-item');
+
+// Album button templates
+ var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
      
@@ -93,5 +100,22 @@ var createSongRow = function(songNumber, songName, songLength) {
              index = 0;
          };
      });
+ 
+     songListContainer.addEventListener('mouseover', function(event) {
+         
+         // Only target individual song rows during event delegation
+         if (event.target.parentElement.className === 'album-view-song-item') {
+             // Change the content from the number to the play button's HTML
+             event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+         }
+     });
+     
+    for (var i = 0; i < songRows.length; i++) {
+        songRows[i].addEventListener('mouseleave', function(event) {
+            // Selects first child element, which is the song-item-number element
+             this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+         });
+     }
+ 
  };
 
