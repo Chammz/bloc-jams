e79a9ae5a092a5d161d486ab475eaf073b60c982
Chammz <topher.hammond@gmail.com>
2016-09-26 16:31:19 -0700

    Checkpoint 16: Complete. Refactored landing.js with Jquery selectors

--- scripts/album.js
+++ scripts/album.js
@@ -85,6 +85,7 @@ var createSongRow = function(songNumber, songName, songLength) {
 var findParentByClassName = function(element, targetClass) {
     if (element) {
         var currentParent = element.parentElement;
+        
         while(currentParent.className != targetClass && currentParent.className !== null) {
             currentParent = currentParent.parentElement;
         }
