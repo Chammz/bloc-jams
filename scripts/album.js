d1f63011bf1d75d7f1433a71c8d719fd069e5df1
Chammz <topher.hammond@gmail.com>
2016-10-16 16:40:18 -0700

    Added changes to checkpoint.Issue with nextSong func and albumImage not defined.

--- scripts/album.js
+++ scripts/album.js
@@ -1,50 +1,3 @@
- // Example Album
- var albumPicasso = {
-     title: 'The Colors',
-     artist: 'Pablo Picasso',
-     label: 'Cubism',
-     year: '1881',
-     albumArtUrl: 'assets/images/album_covers/01.png',
-     songs: [
-         { title: 'Blue', duration: '4:26' },
-         { title: 'Green', duration: '3:14' },
-         { title: 'Red', duration: '5:01' },
-         { title: 'Pink', duration: '3:21'},
-         { title: 'Magenta', duration: '2:15'}
-     ]
- };
- 
- // Another Example Album
- var albumMarconi = {
-     title: 'The Telephone',
-     artist: 'Guglielmo Marconi',
-     label: 'EM',
-     year: '1909',
-     albumArtUrl: 'assets/images/album_covers/20.png',
-     songs: [
-         { title: 'Hello, Operator?', duration: '1:01' },
-         { title: 'Ring, ring, ring', duration: '5:01' },
-         { title: 'Fits in your pocket', duration: '3:21'},
-         { title: 'Can you hear me now?', duration: '3:14' },
-         { title: 'Wrong phone number', duration: '2:15'}
-     ]
- };
-
- var albumMuse = {
-     title: 'Supermassive Blackhole',
-     artist: 'Muse',
-     label: 'Warner Bros.',
-     year: '2006',
-     albumArtUrl: 'assets/images/album_covers/18.png',
-     songs: [
-         { title: 'Take a Bow', duration: '4:35' },
-         { title: 'Starlight', duration: '4:00' },
-         { title: 'Supermassive Black Hole', duration: '3:29'},
-         { title: 'Knights of Cydonia', duration: '6:07' },
-         { title: 'Glorious', duration: '4:41'}
-     ]
- };
-
 var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
@@ -57,42 +10,47 @@ var createSongRow = function(songNumber, songName, songLength) {
      var $row = $(template);
     
      var clickHandler = function(){
-         var songNumber = $(this).attr('data-song-number');
+         var songNumber = parseInt($(this).attr('data-song-number'));
          
-         if(currentlyPlayingSong !== null) {
+         if(currentlyPlayingSongNumber !== null) {
              //Revert to song number for currently playing song because user started playing new song.
-             var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSong + '"]');
-             currentlyPlayingCell.html(currentlyPlayingSong);
+             var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+             currentlyPlayingCell.html(currentlyPlayingSongNumber);
          }
          
-         if(currentlyPlayingSong !== songNumber){
+         if(currentlyPlayingSongNumber !== songNumber){
              //Switch from Play -> Pause button to indicate new song is playing
              $(this).html(pauseButtonTemplate);
-             currentlyPlayingSong = songNumber;
-         } else if (currentlyPlayingSong === songNumber){
+             currentlyPlayingSongNumber = songNumber;
+             currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+             updatePlayerBarSong();
+         } else if (currentlyPlayingSongNumber === songNumber){
              //Switch from Pause -> Play to pause currently playing song.
              $(this).html(playButtonTemplate);
-             currentlyPlayingSong = null;
+             $('.main-controls .play-pause').html(playerBarPlayButton);
+             currentlyPlayingSongNumber = null;
+             currentSongFromAlbum = null;
          }
      };
     
      var onHover = function(event){
          var songNumberCell = $(this).find('.song-item-number');
          //why is data-song-number not selected as a class?
-         var songNumber = songNumberCell.attr('data-song-number')
+         var songNumber = parseInt(songNumberCell.attr('data-song-number'));
          
-         if(songNumber !== currentlyPlayingSong) {
+         if(songNumber !== currentlyPlayingSongNumber) {
              songNumberCell.html(playButtonTemplate);
          }
      };
      
      var offHover = function(event){
         var songNumberCell = $(this).find('.song-item-number');
-        var songNumber = songNumberCell.attr('data-song-number');
+        var songNumber = parseInt(songNumberCell.attr('data-song-number'));
 
-        if (songNumber !== currentlyPlayingSong) {
+        if (songNumber !== currentlyPlayingSongNumber) {
             songNumberCell.html(songNumber);
         }
+
      };
     
     $row.find('.song-item-number').click(clickHandler);
@@ -110,7 +68,10 @@ var createSongRow = function(songNumber, songName, songLength) {
      var $albumSongList = $('.album-view-song-list');
 
  var setCurrentAlbum = function(album) {
-
+     
+     currentAlbum = album;
+     var currentlyPlayingSongNumber = null;
+     var currentSongFromAlbum = null;
      // Assign values to each part of the album (text,images)
      
      $albumTitle.text(album.title);
@@ -130,16 +91,102 @@ var createSongRow = function(songNumber, songName, songLength) {
      }
  };
 
+var trackIndex = function(album, song) {
+    return album.songs.indexOf(song);
+}
+
+var updatePlayerBarSong = function() {
+    $('.currently-playing .song-name').text(currentSongFromAlbum.title);
+    $('.currently-playing .artist-name').text(currentAlbum.artist);
+    $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.title + " - " + currentAlbum.artist);
+    
+    $('.main-controls .play-pause').html(playerBarPauseButton);
+};
+
+
+var nextSong = function () {
+    var getLastSongNumber = function (index) {
+        return index == 0 ? currentAlbum.songs.length : index;
+    };
+    
+    var currentSongIndex = trackIndex(currentAlbum, currentSongFromAlbum);
+    // incremeting song here
+    currentSongIndex++;
+    
+    if (currentSongIndex >= currentAlbum.songs.length) {
+        currentSongIndex = 0;
+    }
+    
+    //set new current song
+    currentlyPlayingSongNumber = currentSongIndex + 1;
+    currentSongFromalbum = currentAlbum.songs[currentSongIndex];
+    
+    //Update the Player Bar Information
+    $('.currentl-playing .song-name').text(currentSongFromAlbum.title);
+    $('.currently-playing .artist-name').text(currentAlbum.artist);
+    $('.main-controls .play-pause').html(playerBarPauseButton);
+    
+    var lastSongNumber = getLastSongNumber(currentSongIndex);
+    var $nextSongNumberCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+    var $lastSongNumberCell = $('.song-item-number[data-song-number="' + lastSongNumber + '"]');
+    
+    $nextSongNumberCell.html(pauseButtonTemplate);
+    $lastSongNumberCell.html(lastSongNumber);
+};
+
+var previousSong = function() {
+    
+    var getLastSongNumber = function(index) {
+        return index == (currentAlbum.songs.length - 1) ? 1 : index + 2;
+    };
+    
+    var currentSongIndex = trackIndex(currentAlbum, currentSongFromAlbum);
+   
+    currentSongIndex--;
+    
+    if (currentSongIndex < 0) {
+        currentSongIndex = currentAlbum.songs.length - 1;
+    }
+    
+    // Set a new current song
+    currentlyPlayingSongNumber = currentSongIndex + 1;
+    currentSongFromAlbum = currentAlbum.songs[currentSongIndex];
+
+    // Update the Player Bar information
+    $('.currently-playing .song-name').text(currentSongFromAlbum.title);
+    $('.currently-playing .artist-name').text(currentAlbum.artist);
+    $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.title + " - " + currentAlbum.title);
+    $('.main-controls .play-pause').html(playerBarPauseButton);
+    
+    var lastSongNumber = getLastSongNumber(currentSongIndex);
+    var $previousSongNumberCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+    var $lastSongNumberCell = $('.song-item-number[data-song-number="' + lastSongNumber + '"]');
+    
+    $previousSongNumberCell.html(pauseButtonTemplate);
+    $lastSongNumberCell.html(lastSongNumber);
+    
+};
 
 // Album button templates
  var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
  var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+ var playerBarPlayButton = '<span class="ion-play"></span>';
+ var playerBarPauseButton = '<span class="ion-pause"></span>';
 
 // Store state of playing songs
- var currentlyPlayingSong = null;
+ var currentAlbum = null;
+ var currentlyPlayingSongNumber = null;
+ var currentSongFromAlbum = null;
+ var $previousButton = $('.main-controls .previous');
+ var $nextButton = $('.main-controls .next');
+
+
+
 
  $(document).ready(function() {
      setCurrentAlbum(albumPicasso);
+     $previousButton.click(previousSong);
+     $nextButton.click(nextSong);
      
      var albums = [albumPicasso, albumMarconi,albumMuse];
      var index = 1;
---
+++ scripts/fixtures.js
@@ -0,0 +1,44 @@
+var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: '4:26' },
+         { title: 'Green', duration: '3:14' },
+         { title: 'Red', duration: '5:01' },
+         { title: 'Pink', duration: '3:21'},
+         { title: 'Magenta', duration: '2:15'}
+     ]
+ };
+ 
+ var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
+
+ var albumMuse = {
+     title: 'Supermassive Blackhole',
+     artist: 'Muse',
+     label: 'Warner Bros.',
+     year: '2006',
+     albumArtUrl: 'assets/images/album_covers/18.png',
+     songs: [
+         { title: 'Take a Bow', duration: '4:35' },
+         { title: 'Starlight', duration: '4:00' },
+         { title: 'Supermassive Black Hole', duration: '3:29'},
+         { title: 'Knights of Cydonia', duration: '6:07' },
+         { title: 'Glorious', duration: '4:41'}
+     ]
+ };
